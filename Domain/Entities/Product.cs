4ed79e8d9ac37e5479c5313f3ce7cc242af1581c
Carles Labrana <carles.labrana@idealiveconsulting.com>
2026-03-30 20:40:15 +0200

    Agregar archivos de proyecto.

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,45 @@
+﻿namespace Domain.Entities;
+
+public class Product
+{
+    private string _name;
+    private decimal _price;
+
+    public Guid Id { get; set; }
+    public string Name { 
+        get => _name; 
+        set {
+            if (string.IsNullOrEmpty(_name))
+            {
+                throw new ArgumentException("Product name cannot be null or empty.");
+            }
+            _name = value;
+        }
+    }
+    public decimal Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Product price cannot be negative.");
+            }
+            _price = value;
+        }
+    }
+
+
+
+
+    public Product(Guid id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+
+
+
+
+}
